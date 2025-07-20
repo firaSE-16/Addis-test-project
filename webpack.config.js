df1604c475c038d638f1ebc433f1d29a7b28fedf
Firaol <loarifse16@gmail.comgit config --global user.email loarifse16@gmail.com>
2025-07-20 18:48:09 +0300

    chore: initialize project and setup manual Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+    entry:"./src/index.js",
+
+    output:{
+        path:path.resolve(__dirname,"dist"),
+        filename:"bundle.js",
+        clean:true,
+        publicPath:"/",
+    },
+
+    mode:"development",
+
+    devServer:{
+        static:path.join(__dirname,"public"),
+        port:8000,
+        hot:true,
+        open:true,
+        historyApiFallback:true,
+    },
+
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:"babel-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test:/\.(png|jpe?g|gif|svg)$/i,
+                type:"asset/resource",
+            }
+        ]
+    },
+
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:"./public/index.html",
+        }),
+        new Dotenv(),
+    ],
+
+    resolve:{
+        extensions:[".js",".jsx"],
+    }
+}
+
+
+
+
+
