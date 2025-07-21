18291805dadee48a482c01baf96ee3820c90ea6c
Firaol <loarifse16@gmail.comgit config --global user.email loarifse16@gmail.com>
2025-07-21 13:59:57 +0300

    fix:webpack error

--- AddisFrontend/webpack.config.js
+++ AddisFrontend/webpack.config.js
@@ -3,66 +3,61 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-    entry:"./src/index.jsx",
+  entry: "./src/index.jsx",
 
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"bundle.js",
-        clean:true,
-        publicPath:"/",
-    },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+    publicPath: "/", 
+  },
 
-    mode:"development",
+  mode: "production", 
 
-    devServer: {
-                static: path.join(__dirname, "public"),
-                port: 9001,
-                hot: true,
-                open: true,
-                historyApiFallback: true,
-                },
-
-    module:{
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use:"babel-loader",
-            },
-            {
-                test: /\.svg$/i,
-                issuer: /\.[jt]sx?$/,
-                use: [
-                    {
-                    loader: '@svgr/webpack',
-                    options: { icon: true },
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test:/\.(png|jpe?g|gif|svg)$/i,
-                type:"asset/resource",
-            }
-        ]
-    },
-
-    plugins:[
-        new HtmlWebpackPlugin({
-            template:"./public/index.html",
-        }),
-        new Dotenv(),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: { icon: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
     ],
+  },
 
-    resolve:{
-        extensions:[".js",".jsx"],
-    }
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new Dotenv(),
+  ],
 
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 
-
-
-
+  devServer: {
+    static: path.join(__dirname, "public"),
+    port: 9001,
+    hot: true,
+    open: true,
+    historyApiFallback: true, 
+  },
+};
