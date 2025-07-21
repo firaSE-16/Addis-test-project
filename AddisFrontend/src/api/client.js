a9ec0207ee63ad2b706eee891c1c45cbe51fd494
Firaol <loarifse16@gmail.comgit config --global user.email loarifse16@gmail.com>
2025-07-21 15:28:54 +0300

    fix:apiclient error

--- AddisFrontend/src/api/client.js
+++ AddisFrontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.API_BASE_URL||'https://addis-test-project-r1hf.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
